fca4e95646aa66cf781c1ec2fba386ce00083a1e
Nadir Fasola <nadir.fasola@gmail.com>
2025-06-26 18:05:54 +0200

    fix: apply rustfmt changes

--- src/core/window.rs
+++ src/core/window.rs
@@ -22,16 +22,13 @@ impl WindowManager {
         gtk4::init()?;
 
         // 1. Create a GTK4 Application with a reverse-domain ID
-        let app = Application::new(
-            Some("com.nadirfasola.panel"),
-            Default::default(),
-        );
+        let app = Application::new(Some("com.nadirfasola.panel"), Default::default());
 
-        // 2. When the app activats, build our panel window
+        // 2. When the app activates, build our panel window
         app.connect_activate(move |app| {
             // 2.a. Create a window tied to the application
             let window = ApplicationWindow::new(app);
-            window.set_default_size(400,30); // 400 x 30 px window
+            window.set_default_size(400, 30); // 400 x 30 px window
             window.set_decorated(false); // remove titlebar
 
             // 2.b. Dock it with layer-shell at the bottom
@@ -39,10 +36,6 @@ impl WindowManager {
             window.set_layer(Layer::Top);
             window.set_anchor(Edge::Bottom, true);
             window.set_exclusive_zone(30);
-            // gtk4_layer_shell::gtk_layer_init_for_window(&window);
-            // gtk4_layer_shell::gtk_layer_set_layer(&window, Layer::Top);
-            // gtk4_layer_shell::gtk_layer_set_anchor(&window, Edge::Bottom, true);
-            // gtk4_layer_shell::gtk_layer_set_exclusive_zone(&window, 30);
 
             // 2.c. Show the window (and all its children)
             window.show();
