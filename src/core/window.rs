0190c1b1e709acfba662dfa8252b6a7e3d52a9d0
Nadir Fasola <nadir.fasola@gmail.com>
2025-06-26 16:37:23 +0200

    feat: implement GTK4 ApplicationWindow and layer-shell docking

--- src/core/window.rs
+++ src/core/window.rs
@@ -1,6 +1,8 @@
 // src/core/window.rs
-
 use anyhow::Result;
+use gtk4::preluse::*;
+use gtk4::{Application, ApplicationWindow};
+use gtk4_layer_shell::{Edge, Layer};
 
 // Manages the panel window lifecycle
 pub struct WindowManager {
@@ -18,7 +20,32 @@ impl WindowManager {
 
     // Builds and runs the panel UI loop
     pub fn run(&mut self) -> Result<()> {
-        // TODO
+        // 1. Create a GTK4 Application with a reverse-domain ID
+        let app = Application::new(
+            Some("com.nadirfasola.panel"),
+            Default::default(),
+        )?;
+
+        // 2. When the app activats, build our panel window
+        app.connect_activate(move |app| {
+            // 2.a. Create a window tied to the application
+            let window = ApplicationWindow::new(app);
+            window.set_default_size(400,30); // 400 x 30 px window
+            window.set_decorated(false); // remove titlebar
+
+            // 2.b. Dock it with layer-shell at the bottom
+            gtk4_layer_shell::init_for_window(&window);
+            gtk4_layer_shell::set_layer(&window, Layer::Top);
+            gtk4_layer_shell::set_anchor(&window, Edge::Bottom, true);
+            gtk4_layer_shell::set_exclusive_zone(&window, 30);
+
+            // 2.c. Show the window (and all its children)
+            window.show();
+        });
+
+        // 3. Run the GTK4 main loop
+        app.run();
+
         Ok(())
     };
 }
